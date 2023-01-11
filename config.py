bb8db1d78ee6e85a9732a6b6c491799e4b4499ac
Alyona <90251910+s-alyona-a@users.noreply.github.com>
2023-01-11 13:02:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///servey.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_APP = "run_flask_app.py"
